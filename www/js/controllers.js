4d2fcb6c2103dff0843bb8aaf07d394b6f0de19f
Jere <t5haje00@students.oamk.fi>
2017-04-03 10:27:15 +0300

    added modals

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,17 +16,39 @@ app.controller('registerCtrl', function(){
 
   });
 
-app.controller('homeCtrl', function($scope){
+app.controller('homeCtrl', function($scope, $ionicModal){
 
     $scope.expand = true;
 
     $scope.examples = [
-      { title: 'Sample 1 ', description: 'This is description 1'},
-      { title: 'Sample 2', description: 'This is description 2'},
-      { title: 'Sample 3', description: 'This is description 3'},
+      { title: 'Title 1 ', description: 'Here goes the full description 1', address: 'someaddress 1', dest_address: 'the destination address 1', delivered_before: '14:00', payment: '10€'},
+      { title: 'Title 2', description: 'This is description 2'},
+      { title: 'Title 3', description: 'This is description 3'},
     ];
 
-  });
+    $scope.Modalopen = function(example){
+      $scope.example = example;
+      $scope.modal.show();
+    }
+
+    $ionicModal.fromTemplateUrl('templates/modal-template.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.Modalclose = function(){
+      $scope.moda.hide();
+    }
+
+});
+
+app.controller('mdlCtrl', function($scope){
+  $scope.Modalhide = function(){
+    $scope.mdlCtrl.hide();
+  };
+
+});
 
 app.controller('newrequestCtrl', function(){
 
